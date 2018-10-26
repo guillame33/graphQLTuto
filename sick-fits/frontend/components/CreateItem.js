e391466efdca7e85f2838d67bb1e2b83b2e18fb1
Dumora Guillaume <g.dumora@lectra.com>
2018-10-26 17:47:49 +0200

    updating done

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import Router from "next/router";
