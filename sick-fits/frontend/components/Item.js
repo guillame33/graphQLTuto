f3283798bbee1907e7190f1ee0a42a1d264efa09
guillaume <gdumora@gmail.com>
2018-10-27 22:36:30 +0200

    delete for front

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -7,6 +7,7 @@ import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
+import DeleteItem from "../components/DeleteItem";
 
 export default class Item extends Component {
   static propTypes = {
@@ -29,7 +30,7 @@ export default class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add to Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete this Item</DeleteItem>
         </div>
       </ItemStyles>
     );
