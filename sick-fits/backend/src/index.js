3457d52dbbaaa68de9377fd9476bca00223b6e6b
Dumora Guillaume <g.dumora@lectra.com>
2018-12-12 09:21:32 +0100

    add authent

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config({ path: "variables.env" });
 
@@ -8,6 +9,16 @@ const db = require("./db");
 const server = createServer();
 
 server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userid onto the request for future requests
+    req.userId = userId;
+  }
+  next();
+});
 // TODO middleware to populate current user
 
 server.start(
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { randomBytes } = require("crypto");
+const { promisify } = require("util");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -49,6 +51,48 @@ const Mutations = {
       maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
     });
     return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // check if there is a user
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // check pass correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid password");
+    }
+    // generate token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // Set the cookie
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // return the user
+    return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Goodbye" };
+  },
+  async requestReset(parent, args, ctx, info) {
+    // Check if its a real user
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) {
+      throw new Error("No User");
+    }
+    // Set a reset token ans expiry
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString("hex");
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    });
+    return { message: "Sent!" };
+    // Email the reset link
   }
 };
 
