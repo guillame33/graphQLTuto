64ef0ea12f27ef7e5efdcc6b2f09c81e1933ed5d
Dumora Guillaume <g.dumora@lectra.com>
2018-12-14 17:03:18 +0100

    add sending mail function

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -2,25 +2,38 @@ import React from "react";
 import Link from "next/link";
 
 import NavStyles from "../components/styles/NavStyles";
+import User from "../components/User";
+import SignOutComponent from "../components/Signout";
 
 const Nav = () => (
-  <NavStyles>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+            <SignOutComponent />
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
---
+++ sick-fits/frontend/pages/signup.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+import SignupComponent from "../components/Signup";
+import SigninComponent from "../components/Signin";
+import RequestReset from "../components/RequestReset";
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+const Signup = props => (
+  <Columns>
+    <SignupComponent />
+    <SigninComponent />
+    <RequestReset />
+  </Columns>
+);
+
+export default Signup;
