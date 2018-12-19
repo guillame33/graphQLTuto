d262099bae576b5382df6593be0b6ac41555dc7a
Dumora Guillaume <g.dumora@lectra.com>
2018-12-19 17:40:47 +0100

    add deletion in cart

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -220,6 +220,24 @@ const Mutations = {
       },
       info
     );
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user {id}}`
+    );
+    // make sure the user own the cart
+    if (!cartItem) throw new Error("No cart Item found");
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("Not Allowed");
+    }
+    // delete the cart item
+    return ctx.db.mutation.deleteCartItem({ where: { id: args.id } }, info);
   }
 };
 
