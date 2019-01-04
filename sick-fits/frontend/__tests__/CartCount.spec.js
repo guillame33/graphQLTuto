e6ec400b96894fb79c4ead71c6b01ca604b68553
Dumora Guillaume <g.dumora@lectra.com>
2019-01-04 16:44:49 +0100

    add snapshot testing

---
+++ sick-fits/frontend/__tests__/CartCount.spec.js
@@ -0,0 +1,21 @@
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+
+import CartCountComponent from "../components/CartCount";
+
+describe("<CartCount />", () => {
+  it("render", () => {
+    shallow(<CartCountComponent count={10} />);
+  });
+  it("matches the snapshot", () => {
+    const wrapper = shallow(<CartCountComponent count={10} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it("update via props", () => {
+    const wrapper = shallow(<CartCountComponent count={50} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+    wrapper.setProps({ count: 10 });
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
